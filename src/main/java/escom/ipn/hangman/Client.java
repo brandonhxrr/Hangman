16787e3b08216d2ff5aaaf27af513344cb7441aa
Brandon Herrera <brandondh769@gmail.com>
2022-09-10 21:27:46 -0500

    Add sockets logic in client and server

--- src/main/java/escom/ipn/hangman/Client.java
+++ src/main/java/escom/ipn/hangman/Client.java
@@ -1,5 +1,100 @@
 package escom.ipn.hangman;
 
+import java.io.BufferedReader;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.util.Arrays;
+import java.util.Scanner;
+import javax.swing.JFileChooser;
+
 public class Client {
+    
+    static Scanner sc = new Scanner(System.in);
+    
+    public static void main (String[] args){
+        try{
+            establecerConexion();
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+    }
+    
+    
+    
+    public static void establecerConexion() throws IOException{
+        int pto = 1234;
+        String dir = "127.0.0.1";
+        Socket cl = new Socket(dir,pto);
+        System.out.println("Conexion con servidor establecida...");
+        
+        BufferedReader br = new BufferedReader(new InputStreamReader(cl.getInputStream()));
+        
+        int length = Integer.parseInt(br.readLine());
+        
+        char[] hidden = new char[length];
+        boolean guessed = false;
+        
+        for (int i = 0; i < length; i++) {
+            hidden[i] = '_';
+        }
+        
+        System.out.println("LONG: " + String.valueOf(length));
+        
+        PrintWriter pw = new PrintWriter( new OutputStreamWriter(cl.getOutputStream()));
+        
+        
+        
+        String guess = "";
+        
+        while(length > 0 && !guessed){
+            System.out.println("Adivina la palabra: ");
+            System.out.println("Número de vidas: " + String.valueOf(length));
+            System.out.println(Arrays.toString(hidden) + "\n");
+            System.out.println("Ingresa una letra: ");
+            char letra = sc.next().charAt(0);
+            pw.print(letra);
+            
+            int answer = Integer.parseInt(br.readLine());
+            
+            
+            
+            if(answer == -1){
+                length -= 1;
+            }else {
+                hidden[answer] = letra;
+            }
+            
+            guessed = !Arrays.asList(hidden).contains('_');
+            
+        }
+        
+        if(guessed) {
+            System.out.println("!Felicidades!");
+        }else {
+            System.out.println("Suerte para la próxima ):");
+        }
+        
+        pw.print("-1");
+        
+        
+        
+        
+        
+        //Receive letters
+        //Validatation loop
+        
+        br.close();
+        pw.close();
+        cl.close();
+        
+        
+    }
     
 }
--- src/main/java/escom/ipn/hangman/Server.java
+++ src/main/java/escom/ipn/hangman/Server.java
@@ -1,5 +1,88 @@
 package escom.ipn.hangman;
 
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.BufferedReader;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.ObjectOutputStream;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.net.StandardSocketOptions;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 public class Server {
-    
+
+  private static Socket socket;
+
+  public static void main(String[] args) {
+      
+      getRequests(getServerSocket(1234));
+ 
+  }
+  
+  private static ServerSocket getServerSocket(int port) {
+      
+      ServerSocket s = null;
+      
+      try {
+          s = new ServerSocket(1234);
+          s.setOption(StandardSocketOptions.SO_REUSEADDR, true);
+          System.out.println("Servidor iniciado...");
+      } catch (IOException ex) {
+          Logger.getLogger(Server.class.getName()).log(Level.SEVERE, null, ex);
+      }
+      
+      return s;
+  }
+  
+  private static void getRequests(ServerSocket s) {
+      for (;;) {
+          try {
+              String[] words = {"animal", "perro", "gato", "vaca", "cerdo", "pecho", "cintura", "cadera"};
+              
+              int x = (int)(Math.random()*(words.length)+1);
+              
+              String word = words[x];
+              Socket cl = s.accept();
+              
+              PrintWriter pw = new PrintWriter( new OutputStreamWriter(cl.getOutputStream()));
+              pw.println(word.length()); // Send the String length
+              //Receive letters
+              
+              boolean end = false;
+              
+              BufferedReader br = new BufferedReader(new InputStreamReader(cl.getInputStream()));
+              
+              while(!end) {
+                  String letra = br.readLine();
+                  System.out.println("RECV: " + letra);
+                  
+                  if(letra == "-1"){
+                      end = true;
+                  }else {
+                      pw.print(word.indexOf(letra));
+                  }
+              }
+              
+              br.close();
+              //Validatation loop
+              pw.close();
+              cl.close();
+              
+          } catch (IOException ex) {
+              Logger.getLogger(Server.class.getName()).log(Level.SEVERE, null, ex);
+          }
+      }
+  }
+  
+
+  
 }
